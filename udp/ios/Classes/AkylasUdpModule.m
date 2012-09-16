6f900de23a14244c4dc3b4605254405b4b18fb05
Martin Guillon <martin.guillon@gmail.com>
2012-09-16 10:22:13 +0200

    udp module
    Former-commit-id: 68cc92fa57d304e4ddd37fd4fef3a1eaa19a6d49
    Former-commit-id: 7ea538ed235fc7030f363272e5727ca9141ebbff

---
+++ udp/ios/Classes/AkylasUdpModule.m
@@ -0,0 +1,23 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+#import "AkylasUdpModule.h"
+
+@implementation AkylasUdpModule
+
+#pragma mark Internal
+
+-(id)moduleGUID
+{
+	return @"0b1bfeae-0991-49c7-aa3b-8d34cfcbfcbf";
+}
+
+-(NSString*)moduleId
+{
+	return @"akylas.udp";
+}
+
+@end
---
+++ udp/ios/Classes/AkylasUdpModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "AkylasUdpModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation AkylasUdpModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
