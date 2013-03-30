b93e3a60f4b9419265fa17507f8add79632e3f26
Martin Guillon <martin.guillon@gmail.com>
2013-03-30 17:58:13 +0100

    updated commonjs module
    Former-commit-id: c6f277b9bd5ceb8bc5c7ec4a2fe2e8f0fc2fd9f2
    Former-commit-id: 13d756fd66cd72a3f79ce3bf35e42b47e502d4dd

--- commonjs/ios/assets/akylas.commonjs.js
+++ commonjs/ios/assets/akylas.commonjs.js
@@ -5,11 +5,7 @@ Ti.API.info('__myPath in commonjs ' + __myPath);
 
 exports.load = function(_modules, _additions, _context) {
 	// this = _context;
-	(function(){
-		var akadditions = _additions;
-		var akmodules = _modules;
-		Ti.include(__myPath  +'AkInclude/akylas.global.js');
-		delete akadditions;
-		delete akmodules;
-	}).call(_context);
+	_context.akadditions = _additions;
+	_context.akmodules = _modules;
+	_context.ak = require(__myPath  +'AkInclude/akylas.global').init(_context);
 };
