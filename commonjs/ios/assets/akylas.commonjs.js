90a059a343df079f29e2f62d78b22e65335edf00
Martin Guillon <martin.guillon@gmail.com>
2013-04-27 10:08:39 +0200

    last version fully working
    Former-commit-id: f14ca42a835e2730c85188d264094b452bb90b1a
    Former-commit-id: 576e5f94c92901c4cfc539a2ec53264bed33d4fc

--- commonjs/ios/assets/akylas.commonjs.js
+++ commonjs/ios/assets/akylas.commonjs.js
@@ -4,8 +4,16 @@ if (__myPath.length > 0 && __myPath[__myPath.length - 1] != '/')
 Ti.API.info('__myPath in commonjs ' + __myPath);
 
 exports.load = function(_modules, _additions, _context) {
-	// this = _context;
 	_context.akadditions = _additions;
 	_context.akmodules = _modules;
 	_context.ak = require(__myPath  +'AkInclude/akylas.global').init(_context);
+	if (_context.ak.ti){
+		var isApple =  Ti.Platform.osname === 'ipad' || Ti.Platform.osname === 'iphone';
+		if (isApple) {
+
+			(function(){
+				ak.ti.loadCreators(__myPath  +'AkWidgets/Notification.js');
+			}).call(_context);
+		}
+	}
 };
