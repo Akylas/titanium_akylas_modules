a28b1bd5d30c6502028b46f957fe583363681a79
Martin Guillon <martin.guillon@gmail.com>
2013-07-29 09:11:41 +0200

    updated commonjs
    Former-commit-id: f438507cd32bd8dffa58e08174e82653f29de652
    Former-commit-id: 6c95101824cf22c599f4e3b9851e7904291f3453

--- commonjs/android/assets/akylas.commonjs.js
+++ commonjs/android/assets/akylas.commonjs.js
@@ -4,7 +4,6 @@ if (__myPath.length > 0 && __myPath[__myPath.length - 1] != '/')
 Ti.API.info('__myPath in commonjs ' + __myPath);
 
 exports.load = function(_modules, _additions, _context) {
-	// this = _context;
 	_context.akadditions = _additions;
 	_context.akmodules = _modules;
 	_context.ak = require(__myPath  +'AkInclude/akylas.global').init(_context);
