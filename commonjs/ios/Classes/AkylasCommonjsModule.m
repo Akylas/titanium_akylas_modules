f5c29e53137eabba6e1f368f94da244df6f8ed8e
Martin Guillon <martin.guillon@akylas.fr>
2015-01-14 08:55:26 +0100

    latest sdk and 64 bit
    Former-commit-id: a8cedcc1a713cba2a14a268d7c77f8b67a6fb6cf
    Former-commit-id: 6b861e76586798f189edbff3940275cbf1fad082

--- commonjs/ios/Classes/AkylasCommonjsModule.m
+++ commonjs/ios/Classes/AkylasCommonjsModule.m
@@ -36,7 +36,7 @@ extern NSString * const TI_APPLICATION_ID;
 -(NSString*) digest:(NSString*)input{
     NSData *data = [input dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
     uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-    CC_SHA1(data.bytes, data.length, digest);
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
     NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
     
     for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
