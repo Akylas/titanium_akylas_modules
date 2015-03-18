6b396b839f370d0325d0c1b319c2a270a94bbdf2
Martin Guillon <martin.guillon@akylas.fr>
2015-03-18 10:35:33 +0100

    new testfairy module
    Former-commit-id: 79055d1aea0a0a85506bfed602f1b2cab29f7d41
    Former-commit-id: 5eda7c5868aa786a113dad7509561562fd4b313d

---
+++ testfairy/ios/Classes/AkylasTestfairyModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "AkylasTestfairyModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation AkylasTestfairyModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
