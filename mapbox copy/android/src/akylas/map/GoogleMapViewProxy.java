227ac06e29e5e4722e350ee0cf1ce9aa1067411d
Martin Guillon <martin.guillon@akylas.fr>
2015-04-14 13:13:54 +0200

    cleanup
    Former-commit-id: 857f56268d267577a75d601f2a8a7fd8a0226fd8 [formerly 2686e47f2b1533173bdc65f340b480fdcce26ca4]
    Former-commit-id: 5b0217c0d388824c276d7464408ca9619f2199ae
    Former-commit-id: 8853482f02526e58ff1144b90d34be301231de36

--- mapbox copy/android/src/akylas/map/GoogleMapViewProxy.java
+++
@@ -1,8 +0,0 @@
-package akylas.map;
-
-import org.appcelerator.kroll.annotations.Kroll;
-
-@Kroll.proxy(creatableInModule = AkylasMapModule.class)
-public class GoogleMapViewProxy extends MapViewProxy {
-
-}
