12b15e4ada90adf41c7dcf9aa2598c9dbf9ac62c
Martin Guillon <martin.guillon@akylas.fr>
2015-06-06 10:15:56 +0200

    tiws
    Former-commit-id: 2e545fb963ea606552f16e55829be24ba8bebce0 [formerly 38fa099da1bfeb724c52b63eb6567b5e747251e2]
    Former-commit-id: 3d114f8100ec2ab5d1d977389d7db7b72567700a
    Former-commit-id: e6b357b5da55b0da315a9f8a88e9de5ae476e115

---
+++ tiws/android/src/net/iamyellow/tiws/WSProxy.java
@@ -0,0 +1,234 @@
+//
+//   Copyright 2012 jordi domenech <jordi@iamyellow.net>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package net.iamyellow.tiws;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.common.Log;
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollProxy;
+import org.appcelerator.titanium.TiBaseActivity;
+import org.appcelerator.titanium.TiContext.OnLifecycleEvent;
+
+import java.lang.ref.WeakReference;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.text.TextUtils;
+
+import com.codebutler.android_websockets.WebSocketClient;
+
+@Kroll.proxy(creatableInModule = TiwsModule.class)
+public class WSProxy extends KrollProxy implements OnLifecycleEvent {
+	private WebSocketClient client;
+	private boolean connected = false;
+
+	// Constructor
+	public WSProxy() {
+		super();
+	}
+
+	// Websocket stuff
+	private void cleanup() {
+		if (client == null || !connected) {
+			return;
+		}
+
+		connected = false;
+		try {
+			client.disconnect();
+		}
+		catch (Exception ex) {
+		}
+		client = null;
+
+		if (TiwsModule.DBG) {
+			Log.d(TiwsModule.LCAT, "* websocket destroyed");
+		}
+	}
+	
+	@Override
+    public void setActivity(Activity activity)
+    {
+        TiBaseActivity oldActivity = (TiBaseActivity) getActivity();
+        TiBaseActivity newActivity = (TiBaseActivity) activity;
+        if (newActivity == oldActivity) return;
+        super.setActivity(activity);
+        
+        if (oldActivity != null) {
+            oldActivity.removeOnLifecycleEventListener(this);
+        }
+        
+        if (newActivity != null) {
+            newActivity.addOnLifecycleEventListener(this);
+        }
+    }
+
+	// Context Lifecycle events
+	@Override
+	public void onStart(Activity activity) {
+	}
+
+	@Override
+	public void onStop(Activity activity) {
+	}
+
+	@Override
+	public void onPause(Activity activity) {
+	}
+
+	@Override
+	public void onResume(Activity activity) {
+	}
+
+	@Override
+	public void onDestroy(Activity activity) {
+		cleanup();
+	}
+
+	// Handle creation options
+	@Override
+	public void handleCreationDict(KrollDict options) {
+		super.handleCreationDict(options);
+	}
+
+	// Methods
+
+	@Kroll.method
+	public void open(Object[] args) {
+		final KrollProxy self = this;
+
+		if (args.length == 0) {
+			throw new IllegalArgumentException("URI argument expected");
+		}
+
+		Object uri = args[0];
+		if (!(uri instanceof String)) {
+			throw new IllegalArgumentException("URI argument must be a string");
+		}
+		String wsUri = (String)uri;
+		List<BasicNameValuePair> extraHeaders = new ArrayList<BasicNameValuePair>();
+		if (args.length > 1) {
+			Object proto = args[1];
+			if (!(proto instanceof Object[])) {
+				throw new IllegalArgumentException("protocols argument must be an array of strings");
+			}
+			Object[] protocols = (Object[])proto;
+			for (int i = 0; i < protocols.length; i++) {
+				if (!(protocols[i] instanceof String)) {
+					throw new IllegalArgumentException("protocol at index " + i + " is not a string");
+				}
+			}
+			BasicNameValuePair protocolHeader = new BasicNameValuePair("Sec-WebSocket-Protocol", TextUtils.join(", ", protocols));
+			extraHeaders.add(protocolHeader);
+		}
+		try {
+			if (TiwsModule.DBG) {
+				Log.d(TiwsModule.LCAT, "* creating websocket");
+			}
+
+			URI wsURI = new URI(wsUri);
+			Log.d(TiwsModule.LCAT, "* URI: " + wsURI);
+			client = new WebSocketClient(wsURI, new WebSocketClient.Listener() {
+				@Override
+				public void onMessage(byte[] data) {
+					if (client == null) {
+						return;
+					}
+				}
+
+				@Override
+				public void onMessage(String message) {
+					if (client == null) {
+						return;
+					}
+
+					KrollDict event = new KrollDict();
+					event.put("data", message);
+					self.fireEvent("message", event);
+				}
+
+				@Override
+				public void onError(Exception error) {
+					if (client == null) {
+						return;
+					}
+
+					if (TiwsModule.DBG) {
+						Log.d(TiwsModule.LCAT, "* websocket error", error);
+					}
+
+					KrollDict event = new KrollDict();
+					event.put("advice", "reconnect");
+					event.put("error", error.toString());
+					self.fireEvent("error", event);
+
+					cleanup();
+				}
+
+				@Override
+				public void onDisconnect(int code, String reason) {
+					if (client == null) {
+						return;
+					}
+
+					if (TiwsModule.DBG) {
+						Log.d(TiwsModule.LCAT, "* creating disconnected; reason = " + reason + "; code = " + String.valueOf(code));
+					}
+					KrollDict event = new KrollDict();
+					event.put("code", code);
+					event.put("reason", reason);
+					self.fireEvent("close", event);
+
+					cleanup();
+				}
+
+				@Override
+				public void onConnect() {
+					connected = true;
+
+					KrollDict event = new KrollDict();
+					self.fireEvent("open", event);
+				}
+			}, extraHeaders);
+
+			client.connect();
+		}
+		catch (URISyntaxException ex) {
+			if (TiwsModule.DBG) {
+				Log.d(TiwsModule.LCAT, "* creating exception", ex);
+			}
+			cleanup();
+		}
+	}
+
+	@Kroll.method
+	public void close() {
+		cleanup();
+	}
+
+	@Kroll.method
+	public void send(String message) {
+		if (client != null && connected) {
+			client.send(message);
+		}
+	}
+}
---
+++ tiws/ios/Classes/NetIamyellowTiwsWSProxy.m
@@ -0,0 +1,148 @@
+//
+//   Copyright 2012 jordi domenech <jordi@iamyellow.net>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+#import "NetIamyellowTiwsWSProxy.h"
+
+@implementation NetIamyellowTiwsWSProxy
+
+-(id)init
+{
+    connected = FALSE;
+    return [super init];
+}
+
+-(void)dealloc
+{
+    if ([WS retainCount] == 1) {
+        // as SRWebScoket library uses ARC, let's be careful releasing the object
+        // so, if retain counter = 1, we are sure nobody else retains it
+        RELEASE_TO_NIL(WS);
+    }
+    [super dealloc];
+}
+
+#pragma WebSocket Delegate
+
+-(void)webSocketDidOpen:(SRWebSocket*)webSocket
+{
+    connected  = YES;
+
+    if ([self _hasListeners:@"open"]) {
+        [self fireEvent:@"open" withObject:nil];
+    }
+}
+
+-(void)webSocket:(SRWebSocket*)webSocket didFailWithError:(NSError*)error
+{
+    connected  = NO;
+
+    if ([self _hasListeners:@"error"]) {
+        NSDictionary* event = [NSDictionary dictionaryWithObjectsAndKeys:
+                               @"reconnect",@"advice",
+                               [error description],@"error",
+                               nil];
+
+        [self fireEvent:@"error" withObject:event];
+    }
+}
+
+-(void)webSocket:(SRWebSocket*)webSocket didCloseWithCode:(NSInteger)code reason:(NSString*)reason wasClean:(BOOL)wasClean
+{
+    connected = NO;
+
+    if ([self _hasListeners:@"close"]) {
+        NSDictionary* event = [NSDictionary dictionaryWithObjectsAndKeys:NUMINT(code),@"code",reason,@"reason",nil];
+        [self fireEvent:@"close" withObject:event];
+    }
+}
+
+-(void)webSocket:(SRWebSocket*)webSocket didReceiveMessage:(id)data
+{
+    if ([self _hasListeners:@"message"]) {
+        NSDictionary* event = [NSDictionary dictionaryWithObjectsAndKeys:data,@"data",nil];
+        [self fireEvent:@"message" withObject:event];
+    }
+}
+
+#pragma Public API
+
+-(void)open:(id)args
+{
+    if (WS || connected) {
+        return;
+    }
+
+    id url = nil;
+    ENSURE_ARG_AT_INDEX(url, args, 0, NSString);
+    id protocols = nil;
+    ENSURE_ARG_OR_NIL_AT_INDEX(protocols, args, 1, NSArray);
+    id headers = nil;
+    ENSURE_ARG_OR_NIL_AT_INDEX(headers, args, 2, NSDictionary);
+
+    NSMutableURLRequest* req = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+    if (headers) {
+        for (NSString* header in headers) {
+            [req setValue:[headers objectForKey:header] forHTTPHeaderField:header];
+        }
+    }
+
+    WS = [[SRWebSocket alloc] initWithURLRequest:req protocols:protocols];
+    WS.delegate = self;
+    [WS open];
+}
+
+
+- (void)reconnect:(id)args
+{
+    WS.delegate = nil;
+    [WS close];
+
+    id url = nil;
+    ENSURE_ARG_AT_INDEX(url, args, 0, NSString);
+    id protocols = nil;
+    ENSURE_ARG_OR_NIL_AT_INDEX(protocols, args, 1, NSArray);
+
+    WS = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]] protocols:protocols];
+    WS.delegate = self;
+    [WS open];
+}
+
+-(void)close:(id)args
+{
+    if (WS && connected) {
+        [WS close];
+
+    }
+}
+
+-(void)send:(id)msg
+{
+    ENSURE_SINGLE_ARG(msg, NSString);
+
+    if (WS && connected) {
+        [WS send:msg];
+    }
+}
+
+-(NSNumber*)readyState
+{
+    if (WS == nil) {
+        return [NSNumber numberWithInt:-1];
+    }
+    return [NSNumber numberWithInt:WS.readyState];
+}
+
+@end
