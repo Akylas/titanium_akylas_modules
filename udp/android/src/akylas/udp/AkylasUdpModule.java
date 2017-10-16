15d9c18d69bb713438c1fc1d918efc4adf94cfb7
Martin Guillon <martin.guillon@akylas.fr>
2017-10-16 13:10:07 +0200

    fix
    Former-commit-id: 68a6c34e198e525eedc076ae2b09c513d4c76829
    Former-commit-id: ff645663ca6ca25df895440aae59e5b44c3f28d6 [formerly abd3c314df7b46946ed3c8e4a7b24cebdd9aa388]
    Former-commit-id: 6c06be1825d40edd5df371b725dbf55a4051cd0d
    Former-commit-id: 838754f3ddb648878bfefe739098099161821c3e

--- udp/android/src/akylas/udp/AkylasUdpModule.java
+++ udp/android/src/akylas/udp/AkylasUdpModule.java
@@ -3,10 +3,21 @@ package akylas.udp;
 
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.titanium.TiApplication;
+
+import android.app.Activity;
 
 @Kroll.module(name = "AkylasUdp", id = "akylas.udp")
 public class AkylasUdpModule extends KrollModule {
 	public AkylasUdpModule() {
 		super();
 	}
+	
+	static public Activity getCurrentOrRootActivity() {
+        Activity activity = TiApplication.getAppCurrentActivity();
+        if (activity == null) {
+            return TiApplication.getAppRootOrCurrentActivity();
+        }
+        return activity;
+    }
 }
